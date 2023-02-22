434c8980b3a527081cb0cacb27cf21da37728529
DooNas <jaejun.lee@ahha.ai>
2023-02-22 12:14:42 +0900

    상위 디렉토리를 기반으로 하위 디렉토리 자동 선택 - 하위 디렉토리도 분류해서 한다는 점이 이상하다고 판단

--- AutoRemoveFile/MainForm.Designer.cs
+++ AutoRemoveFile/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace AutoRemoveFile
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// 필수 디자이너 변수입니다.
@@ -28,7 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.cb_AutoStart = new System.Windows.Forms.CheckBox();
             this.lb_log = new System.Windows.Forms.Label();
             this.rtb_log = new System.Windows.Forms.RichTextBox();
             this.panel2 = new System.Windows.Forms.Panel();
@@ -39,29 +41,51 @@
             this.lb_Path = new System.Windows.Forms.Label();
             this.tb_Path = new System.Windows.Forms.TextBox();
             this.panel3 = new System.Windows.Forms.Panel();
-            this.clb_Delete = new System.Windows.Forms.CheckedListBox();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.lb_hour = new System.Windows.Forms.Label();
+            this.tb_lastupdate = new System.Windows.Forms.TextBox();
+            this.bt_setpath = new System.Windows.Forms.Button();
             this.lb_Delete = new System.Windows.Forms.Label();
+            this.lb_lastupdate = new System.Windows.Forms.Label();
             this.lb_Tree_Directory = new System.Windows.Forms.Label();
             this.Tree_Directory = new System.Windows.Forms.TreeView();
             this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
             this.fileSystemWatcher = new System.IO.FileSystemWatcher();
+            this.Tray_Icon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.Context_TaryIcon = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.sm_show = new System.Windows.Forms.ToolStripMenuItem();
+            this.sm_exit = new System.Windows.Forms.ToolStripMenuItem();
+            this.bt_logPath = new System.Windows.Forms.Button();
+            this.listb_deletePath = new System.Windows.Forms.ListBox();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.panel3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher)).BeginInit();
+            this.Context_TaryIcon.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.bt_logPath);
+            this.panel1.Controls.Add(this.cb_AutoStart);
             this.panel1.Controls.Add(this.lb_log);
             this.panel1.Controls.Add(this.rtb_log);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel1.Location = new System.Drawing.Point(0, 456);
+            this.panel1.Location = new System.Drawing.Point(0, 455);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(806, 218);
+            this.panel1.Size = new System.Drawing.Size(806, 245);
             this.panel1.TabIndex = 0;
+            // 
+            // cb_AutoStart
+            // 
+            this.cb_AutoStart.AutoSize = true;
+            this.cb_AutoStart.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.cb_AutoStart.Location = new System.Drawing.Point(693, 217);
+            this.cb_AutoStart.Name = "cb_AutoStart";
+            this.cb_AutoStart.Size = new System.Drawing.Size(99, 19);
+            this.cb_AutoStart.TabIndex = 3;
+            this.cb_AutoStart.Text = "AutoStart :";
+            this.cb_AutoStart.UseVisualStyleBackColor = true;
+            this.cb_AutoStart.CheckedChanged += new System.EventHandler(this.AutoStart);
             // 
             // lb_log
             // 
@@ -77,7 +101,7 @@
             // 
             this.rtb_log.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.rtb_log.Location = new System.Drawing.Point(12, 39);
+            this.rtb_log.Location = new System.Drawing.Point(12, 40);
             this.rtb_log.Name = "rtb_log";
             this.rtb_log.Size = new System.Drawing.Size(783, 167);
             this.rtb_log.TabIndex = 0;
@@ -122,9 +146,9 @@
             // tb_Time
             // 
             this.tb_Time.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.tb_Time.Location = new System.Drawing.Point(655, 12);
+            this.tb_Time.Location = new System.Drawing.Point(686, 12);
             this.tb_Time.Name = "tb_Time";
-            this.tb_Time.Size = new System.Drawing.Size(69, 25);
+            this.tb_Time.Size = new System.Drawing.Size(38, 25);
             this.tb_Time.TabIndex = 2;
             this.tb_Time.Text = "3";
             this.tb_Time.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
@@ -134,7 +158,7 @@
             this.lb_Interval.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.lb_Interval.AutoSize = true;
             this.lb_Interval.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.lb_Interval.Location = new System.Drawing.Point(542, 15);
+            this.lb_Interval.Location = new System.Drawing.Point(575, 15);
             this.lb_Interval.Name = "lb_Interval";
             this.lb_Interval.Size = new System.Drawing.Size(117, 20);
             this.lb_Interval.TabIndex = 3;
@@ -163,57 +187,73 @@
             // 
             // panel3
             // 
-            this.panel3.Controls.Add(this.clb_Delete);
-            this.panel3.Controls.Add(this.button2);
-            this.panel3.Controls.Add(this.button1);
+            this.panel3.Controls.Add(this.listb_deletePath);
+            this.panel3.Controls.Add(this.lb_hour);
+            this.panel3.Controls.Add(this.tb_lastupdate);
+            this.panel3.Controls.Add(this.bt_setpath);
             this.panel3.Controls.Add(this.lb_Delete);
+            this.panel3.Controls.Add(this.lb_lastupdate);
             this.panel3.Controls.Add(this.lb_Tree_Directory);
             this.panel3.Controls.Add(this.Tree_Directory);
             this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel3.Location = new System.Drawing.Point(0, 47);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(806, 409);
+            this.panel3.Size = new System.Drawing.Size(806, 408);
             this.panel3.TabIndex = 2;
             // 
-            // clb_Delete
+            // lb_hour
             // 
-            this.clb_Delete.FormattingEnabled = true;
-            this.clb_Delete.Location = new System.Drawing.Point(416, 43);
-            this.clb_Delete.Name = "clb_Delete";
-            this.clb_Delete.Size = new System.Drawing.Size(379, 304);
-            this.clb_Delete.TabIndex = 5;
+            this.lb_hour.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lb_hour.AutoSize = true;
+            this.lb_hour.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lb_hour.Location = new System.Drawing.Point(489, 374);
+            this.lb_hour.Name = "lb_hour";
+            this.lb_hour.Size = new System.Drawing.Size(47, 20);
+            this.lb_hour.TabIndex = 6;
+            this.lb_hour.Text = "Hour";
             // 
-            // button2
+            // tb_lastupdate
             // 
-            this.button2.Anchor = System.Windows.Forms.AnchorStyles.Right;
-            this.button2.Location = new System.Drawing.Point(416, 353);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(379, 50);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.tb_lastupdate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.tb_lastupdate.Location = new System.Drawing.Point(451, 372);
+            this.tb_lastupdate.Name = "tb_lastupdate";
+            this.tb_lastupdate.Size = new System.Drawing.Size(36, 25);
+            this.tb_lastupdate.TabIndex = 6;
+            this.tb_lastupdate.Text = "48";
+            this.tb_lastupdate.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
-            // button1
+            // bt_setpath
             // 
-            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Location = new System.Drawing.Point(12, 353);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(379, 50);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.bt_setpath.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.bt_setpath.Location = new System.Drawing.Point(12, 353);
+            this.bt_setpath.Name = "bt_setpath";
+            this.bt_setpath.Size = new System.Drawing.Size(268, 50);
+            this.bt_setpath.TabIndex = 3;
+            this.bt_setpath.Text = "CHECK";
+            this.bt_setpath.UseVisualStyleBackColor = true;
+            this.bt_setpath.Click += new System.EventHandler(this.bt_Path_Click);
             // 
             // lb_Delete
             // 
             this.lb_Delete.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.lb_Delete.AutoSize = true;
             this.lb_Delete.Font = new System.Drawing.Font("굴림", 12F);
-            this.lb_Delete.Location = new System.Drawing.Point(419, 15);
+            this.lb_Delete.Location = new System.Drawing.Point(305, 15);
             this.lb_Delete.Name = "lb_Delete";
             this.lb_Delete.Size = new System.Drawing.Size(101, 20);
             this.lb_Delete.TabIndex = 3;
             this.lb_Delete.Text = "Delete List";
+            // 
+            // lb_lastupdate
+            // 
+            this.lb_lastupdate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lb_lastupdate.AutoSize = true;
+            this.lb_lastupdate.Font = new System.Drawing.Font("굴림", 12F);
+            this.lb_lastupdate.Location = new System.Drawing.Point(300, 374);
+            this.lb_lastupdate.Name = "lb_lastupdate";
+            this.lb_lastupdate.Size = new System.Drawing.Size(159, 20);
+            this.lb_lastupdate.TabIndex = 6;
+            this.lb_lastupdate.Text = "LAST UPDATE : ";
             // 
             // lb_Tree_Directory
             // 
@@ -233,8 +273,9 @@
             this.Tree_Directory.CheckBoxes = true;
             this.Tree_Directory.Location = new System.Drawing.Point(12, 43);
             this.Tree_Directory.Name = "Tree_Directory";
-            this.Tree_Directory.Size = new System.Drawing.Size(379, 304);
+            this.Tree_Directory.Size = new System.Drawing.Size(268, 303);
             this.Tree_Directory.TabIndex = 0;
+            this.Tree_Directory.AfterCheck += new System.Windows.Forms.TreeViewEventHandler(this.tvdir_AfterCheck);
             // 
             // openFileDialog
             // 
@@ -245,16 +286,62 @@
             this.fileSystemWatcher.EnableRaisingEvents = true;
             this.fileSystemWatcher.SynchronizingObject = this;
             // 
-            // Form1
+            // Tray_Icon
+            // 
+            this.Tray_Icon.ContextMenuStrip = this.Context_TaryIcon;
+            this.Tray_Icon.Text = "파일정리";
+            this.Tray_Icon.Visible = true;
+            // 
+            // Context_TaryIcon
+            // 
+            this.Context_TaryIcon.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.Context_TaryIcon.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sm_show,
+            this.sm_exit});
+            this.Context_TaryIcon.Name = "Context_TaryIcon";
+            this.Context_TaryIcon.Size = new System.Drawing.Size(116, 52);
+            // 
+            // sm_show
+            // 
+            this.sm_show.Name = "sm_show";
+            this.sm_show.Size = new System.Drawing.Size(115, 24);
+            this.sm_show.Text = "Show";
+            // 
+            // sm_exit
+            // 
+            this.sm_exit.Name = "sm_exit";
+            this.sm_exit.Size = new System.Drawing.Size(115, 24);
+            this.sm_exit.Text = "Exit";
+            // 
+            // bt_logPath
+            // 
+            this.bt_logPath.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.bt_logPath.Location = new System.Drawing.Point(698, 11);
+            this.bt_logPath.Name = "bt_logPath";
+            this.bt_logPath.Size = new System.Drawing.Size(97, 25);
+            this.bt_logPath.TabIndex = 6;
+            this.bt_logPath.Text = "PATH";
+            this.bt_logPath.UseVisualStyleBackColor = true;
+            // 
+            // listb_deletePath
+            // 
+            this.listb_deletePath.FormattingEnabled = true;
+            this.listb_deletePath.ItemHeight = 15;
+            this.listb_deletePath.Location = new System.Drawing.Point(302, 43);
+            this.listb_deletePath.Name = "listb_deletePath";
+            this.listb_deletePath.Size = new System.Drawing.Size(493, 319);
+            this.listb_deletePath.TabIndex = 7;
+            // 
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(806, 674);
+            this.ClientSize = new System.Drawing.Size(806, 700);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "MainForm";
+            this.Text = "AutoRemoveFile";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.panel2.ResumeLayout(false);
@@ -262,6 +349,7 @@
             this.panel3.ResumeLayout(false);
             this.panel3.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher)).EndInit();
+            this.Context_TaryIcon.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -283,10 +371,18 @@
         private System.Windows.Forms.Label lb_log;
         private System.Windows.Forms.Label lb_Tree_Directory;
         private System.Windows.Forms.Label lb_Delete;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button bt_setpath;
         private System.Windows.Forms.Button bt_Check;
-        private System.Windows.Forms.CheckedListBox clb_Delete;
+        private System.Windows.Forms.Label lb_lastupdate;
+        private System.Windows.Forms.Label lb_hour;
+        private System.Windows.Forms.TextBox tb_lastupdate;
+        private System.Windows.Forms.CheckBox cb_AutoStart;
+        private System.Windows.Forms.NotifyIcon Tray_Icon;
+        private System.Windows.Forms.ContextMenuStrip Context_TaryIcon;
+        private System.Windows.Forms.ToolStripMenuItem sm_show;
+        private System.Windows.Forms.ToolStripMenuItem sm_exit;
+        private System.Windows.Forms.Button bt_logPath;
+        private System.Windows.Forms.ListBox listb_deletePath;
     }
 }
 
--- AutoRemoveFile/MainForm.cs
+++ AutoRemoveFile/MainForm.cs
@@ -7,12 +7,28 @@ using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace AutoRemoveFile
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
+            this.Load += Tray_Icon_Load;
+            Tray_Icon.MouseDoubleClick += Tray_Icon_MouseDoubleClick;
         }
+        #region 트레이 아이콘
+        private void Tray_Icon_Load(object sender, EventArgs e)
+        {
+            Tray_Icon.ContextMenuStrip = Context_TaryIcon;
+        }
+        private void Tray_Icon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.ShowInTaskbar= true;
+            this.Visible = true;
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        #endregion
+
         #region 원하는 상위 디렉토리 기반으로 TreeView 구현
         private void tb_Path_Click(object sender, EventArgs e)
         {
@@ -56,16 +72,23 @@ namespace AutoRemoveFile
             }
             return directoryNode;
         }
+        private void tvdir_AfterCheck(object sender, TreeViewEventArgs e)
+        {
+            var node = e.Node;
+
+            var children = node.Nodes;
+            for (int K = 0; K < children.Count; K++) { children[K].Checked = node.Checked; }
+        }
 
         #endregion
 
-
+        #region TreeView에서 선택한 경로를 Delete List로 연결
         private List<TreeNode> checkedNodes = new List<TreeNode>();
-
-        private void button1_Click(object sender, EventArgs e)
+        private void bt_Path_Click(object sender, EventArgs e)
         {
             checkedNodes.Clear();//초기화
             CheckedNodes(Tree_Directory.Nodes);
+            AddListBox_DeletePath(checkedNodes);
         }
         private void CheckedNodes(TreeNodeCollection nodes)//재귀를 활용해서 자식까지 진행
         {
@@ -75,7 +98,23 @@ namespace AutoRemoveFile
                 else CheckedNodes(node.Nodes);
             }
         }
+        private void AddListBox_DeletePath(List<TreeNode> nodes)
+        {
+            listb_deletePath.Items.Clear();
+            foreach(TreeNode node in nodes)
+            {
+                string path = node.FullPath;
+                listb_deletePath.Items.Add(path);
+            }
+        }
+        #endregion
 
+        #region 윈도우 부팅시 자동시작
+        private void AutoStart(object sender, EventArgs e)
+        {
 
+        }
+
+        #endregion
     }
 }
